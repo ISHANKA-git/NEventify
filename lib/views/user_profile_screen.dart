14fba7fe65e0dafc9780d07747a32e20743946a9
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-12 14:23:04 +0530

    Update

--- lib/views/user_profile_screen.dart
+++ lib/views/user_profile_screen.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: UserProfileScreen(),
-    );
-  }
-}
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
