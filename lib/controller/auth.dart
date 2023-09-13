fa651f4cb0353994c1caa6608b3783df49f436b5
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-13 16:50:40 +0530

    Update auth.dart

--- lib/controller/auth.dart
+++ lib/controller/auth.dart
@@ -1,2 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthServices{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future signInAnonymously() async{
+    UserCredential result = await _auth.signInAnonymously();
+    User? user = result.user;
+    return user;
+
+  }
+}
