6c63519150ef293225498a05210f9be584bc9180
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-16 21:37:33 +0530

    Create Stream

--- lib/controller/auth.dart
+++ lib/controller/auth.dart
@@ -2,11 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:n_eventify/model/user_model.dart';
 
 class AuthServices {
+  //Firebase instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
   UserModel? userWithFirebaseUserUid(User? user){
     return user != null ? UserModel(uid: user.uid) : null;
   }
-
+  //Create Stream
+  Stream<UserModel?> get user{
+    return _auth.authStateChanges().map(userWithFirebaseUserUid);
+  }
+  //Sign in Anonymous
   Future signInAnonymously() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
