7a8a5ee82c5e47074321a9618596b26ba3b596a7
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-16 20:49:59 +0530

    Create User Model

--- lib/controller/auth.dart
+++ lib/controller/auth.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:n_eventify/model/user_model.dart';
 
 class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  UserModel? userWithFirebaseUserUid(User? user){
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
 
   Future signInAnonymously() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return userWithFirebaseUserUid(user);
     } catch (err) {
       print(err.toString());
       return null;
