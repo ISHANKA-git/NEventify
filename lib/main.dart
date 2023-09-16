51b07fb3a8f78f7188ea7d6242e86ef4b505d52f
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-16 21:54:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:n_eventify/controller/auth.dart';
 import 'package:n_eventify/controller/wrapper.dart';
+import 'package:n_eventify/model/user_model.dart';
 import 'package:n_eventify/views/bottom_navigation_bar.dart';
 import 'package:n_eventify/views/login_signup.dart';
 import 'package:n_eventify/views/home_page.dart';
@@ -9,6 +11,7 @@ import 'package:n_eventify/views/onboarding_screen.dart';
 import 'package:n_eventify/views/event_creation_page.dart';
 import 'package:n_eventify/views/splash_page.dart';
 import 'package:n_eventify/views/chat_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,9 +32,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+    return StreamProvider<UserModel?>.value(
+        initialData: UserModel(uid: ""),
+        value: AuthServices().user,
+        child : MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+        ),
     );
   }
 }
