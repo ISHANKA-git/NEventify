0d207067a8d79cec7b3615421f4f5673041229b6
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-09-16 20:51:47 +0530

    Revert "Merge branch 'main' of https://github.com/ISHANKA-git/NEventify"
    This reverts commit 253a2f672088892f22dce71e1a9bfb9abec49103, reversing
    changes made to 413f4b9394014d74f52dffe0ceb350ffa8eae72a.

--- lib/views/account_details.dart
+++ lib/views/account_details.dart
@@ -121,6 +121,8 @@ class AccountDetailScreen extends StatelessWidget {
 }
 
 class UploadPhoto extends StatelessWidget {
+  const UploadPhoto({super.key});
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -144,95 +146,8 @@ class UploadPhoto extends StatelessWidget {
         ),
       ),
     ),
-          body: SingleChildScrollView(
-          child: Padding(
-          padding: const EdgeInsets.only(left: 30, right: 30),
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          SizedBox(
-          height: 30,
-        ),
-        Text(
-          'Upload your photo profile',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-          ),
-        ),
-          SizedBox(height: 15),
-        Text(
-          'This data will be displayed in your acccount profile for security',
-          style: TextStyle(fontSize: 16),
-        ),
-          SizedBox(height: 60),
-        Center(
-          child: Stack(
-            alignment: Alignment.topRight,
-            children: [
-              Container(
-                width: 150, // Adjust the size of the profile photo container
-                height: 150, // Adjust the size of the profile photo container
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  border: Border.all(
-                    color: Color(0xFF00BF63),
-                    width: 2,
-                  ),
-                ),
-                child: CircleAvatar(
-                  // Your profile photo widget goes here
-                  backgroundImage: NetworkImage('your_profile_image_url'),
-                  // You can use AssetImage or FileImage based on your needs
-                ),
-              ),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {
-                      // Implement the logic to remove the profile photo here
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-          const SizedBox(height: 150.0),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                // Implement the logic to navigate to the next screen or perform an action.
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(110, 45),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                primary: Color(0xFF00BF63),
-              ),
-              child: const Text(
-                'Next',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          )
-          ],
-      ),
-          ),
-          ),
-      ),
-    ),
+      )
     );
   }
 }
+
