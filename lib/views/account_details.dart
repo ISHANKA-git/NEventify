c8a141086e58c773eac991c8340f366637810dfa
Zupuu <121217903+Zupuu@users.noreply.github.com>
2023-09-18 11:38:02 +0530

    create photo profile screen

--- lib/views/account_details.dart
+++ lib/views/account_details.dart
@@ -93,7 +93,7 @@ class AccountDetailScreen extends StatelessWidget {
                   Center(
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){return UploadPhoto();},),);
+                        Navigator.push(context, MaterialPageRoute(builder: (context){return photoprofile();},),);
 
                       },
                       style: ElevatedButton.styleFrom(
@@ -109,7 +109,7 @@ class AccountDetailScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
@@ -119,6 +119,100 @@ class AccountDetailScreen extends StatelessWidget {
     );
   }
 }
+
+class photoprofile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFC8E6C9), // Green background color for the icon
+                    shape: BoxShape.circle, // Rounded shape
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back_ios_sharp, color: Color(0xFF00BF63)),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Navigate back to previous page
+                    },
+                  ),
+                ),
+              ),
+            ),
+            body: SingleChildScrollView(
+            child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Container(
+        height: MediaQuery.of(context).size.height,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    SizedBox(
+    height: 30,
+    ),
+    Text(
+    'Upload your photo profile',
+    style: TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    ),
+    ),
+    SizedBox(height: 15),
+    Text(
+    'This data will be displayed in your acccount profile for security',
+    style: TextStyle(fontSize: 16),
+    ),
+
+    const SizedBox(height: 20.0),
+    Center(
+    child: ElevatedButton(
+    onPressed: () {
+    Navigator.push(context, MaterialPageRoute(builder: (context){return UploadPhoto();},),);
+
+    },
+    style: ElevatedButton.styleFrom(
+    fixedSize: const Size(110, 45),
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(15)),
+    primary: Color(0xFF00BF63),
+    ),
+    child: const Text(
+    'Next',
+    style: TextStyle(
+    fontSize: 16,
+    ),
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
+    ),
+    ),
+    ),
+    );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class UploadPhoto extends StatelessWidget {
   @override
