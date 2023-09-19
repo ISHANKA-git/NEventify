2616469afa9ea0e4cc303b1da71bd4c660aa6d3f
rajithanimansa <97370413+rajithanimansa@users.noreply.github.com>
2023-09-19 14:11:14 +0530

    update

--- lib/views/user_profile_screen.dart
+++ lib/views/user_profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:n_eventify/controller/auth.dart';
 import 'package:n_eventify/views/login_signup.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 
 class UserProfileScreen extends StatefulWidget {
@@ -57,8 +60,384 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       if (currentImageIndex >= images.length) {
         currentImageIndex = 0; // Loop back to the first image
       }
+
+
     });
   }
+
+
+  //rate app
+  void showRatingDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+
+        return AlertDialog(
+          title: const Center(
+            child: Text(
+              "Did you enjoy the app?",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text("Give us a quick rating so we know if you like it?"),
+              const SizedBox(height: 20),
+              RatingBar.builder(
+                initialRating: 5, // Initial rating value (5 stars)
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: false,
+                itemCount: 5,
+                itemSize: 40,
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                onRatingUpdate: (rating) async {
+                  // Handle the rating and open the Google Play Store
+                  final playStoreUrl = 'YOUR_GOOGLE_PLAY_STORE_LINK'; // Replace with your app's Play Store URL
+                  if (rating >= 4.0) {
+                    if (await canLaunch(playStoreUrl)) {
+                      await launch(playStoreUrl);
+                    } else {
+                      throw 'Could not launch $playStoreUrl';
+                    }
+                  }
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+
+
+  // Function to open more apps in the Play Store
+  void openMoreApps() async {
+    const playStoreUrl = 'https://play.google.com/store/apps/developer?id=Your+Developer+Name';
+
+    if (await canLaunch(playStoreUrl)) {
+      await launch(playStoreUrl);
+    } else {
+      throw 'Could not launch $playStoreUrl';
+    }
+  }
+
+
+
+
+  // Function to share the app
+  void shareApp(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.textsms,
+                color: Colors.blueGrey, // Gmail icon with red color
+              ),
+              title: const Text('Share via SMS'),
+              onTap: () {
+                Share.share('Check out my awesome app: YOUR_APP_NAME via SMS');
+                Navigator.of(context).pop(); // Close the modal sheet
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.email,
+                color: Colors.grey, // Gmail icon with red color
+              ),
+              title: const Text('Share via Email'),
+              onTap: () {
+                Share.share('Check out my awesome app: YOUR_APP_NAME via Email');
+                Navigator.of(context).pop(); // Close the modal sheet
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.bluetooth,
+                color: Colors.blue, // Bluetooth icon with blue color
+              ),
+              title: const Text("Bluetooth"),
+              onTap: () {
+                // Handle Bluetooth functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.copy,
+                color: Colors.green, // Copy to Clipboard icon with green color
+              ),
+              title: const Text("Copy to Clipboard"),
+              onTap: () {
+                // Handle Copy to Clipboard functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.wifi,
+                color: Colors.orange, // Wi-Fi Direct icon with orange color
+              ),
+              title: const Text("Wi-Fi Direct"),
+              onTap: () {
+                // Handle Wi-Fi Direct functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.message,
+                color: Colors.green, // WhatsApp icon with green color
+              ),
+              title: const Text("WhatsApp"),
+              onTap: () {
+                // Handle WhatsApp functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.near_me,
+                color: Colors.blue, // Nearby Share icon with blue color
+              ),
+              title: const Text("Nearby Share"),
+              onTap: () {
+                // Handle Nearby Share functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.chat,
+                color: Colors.blue, // Chats icon with blue color
+              ),
+              title: const Text("Chats"),
+              onTap: () {
+                // Handle Chats functionality
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(
+                Icons.mail,
+                color: Colors.red, // Gmail icon with red color
+              ),
+              title: const Text("Gmail"),
+              onTap: () {
+                // Handle Gmail functionality
+              },
+            ),
+
+
+            // Add more sharing options as needed
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+
+
+
+
+  // Function to provide feedback
+  void showFeedbackDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Row(
+            children: [
+              Text("Your Valuable Feedback "),
+              Icon(
+                Icons.sentiment_satisfied, // Add a smiley emoji or any other icon here
+                color: Colors.green, // Customize the icon color
+              ),
+            ],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text("Please enter your feedback below:"),
+              const SizedBox(height: 10),
+              TextFormField(
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Your feedback here',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () async {
+                  const email = 'your@email.com'; // Replace with your feedback email
+                  const subject = 'Feedback for YOUR_APP_NAME'; // Replace with your app's name
+
+                  final url = 'mailto:$email?subject=$subject';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: const Text("Submit"),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // Function to open the privacy policy
+  void openPrivacyPolicyDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Google Privacy Policy Agreement',
+            style: TextStyle(color: Colors.white),
+          ),
+          content: const Text(
+            'You must agree with the Google Privacy Policy!',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'View Privacy Policy',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onPressed: () async {
+                const privacyPolicyUrl = 'https://policies.google.com/privacy';
+                if (await canLaunch(privacyPolicyUrl)) {
+                  await launch(privacyPolicyUrl);
+                } else {
+                  throw 'Could not launch $privacyPolicyUrl';
+                }
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Close',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+  // Function to check for updates
+  void checkForUpdates(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Upgrade Now!',
+            style: TextStyle(color: Colors.red),
+          ),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'No other upgrades available for Nventify ',
+                style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.sentiment_satisfied,
+                    color: Colors.green,
+                    size: 30,
+                  ),
+                  Text(
+                    ' 😊',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
+            ]
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
   final AuthServices _auth = AuthServices();
   @override
   Widget build(BuildContext context) {
@@ -66,43 +445,44 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         actions: [
-          ElevatedButton(onPressed: () async {
-            Navigator.push(context, MaterialPageRoute(builder: (context){return Login_SignUp();},),);
 
-            await _auth.signOut();
-          }, child: Icon(Icons.logout),)
+          Container(
+            width: 40,
+            height: 40,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.message, color: Colors.greenAccent),
+              onPressed: () {
+                Scaffold.of(context).openDrawer(); // Handle message button click
+              },
+             
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.white, width: 2.0),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.list,color: Colors.greenAccent),
+              onPressed: () {
+                // Handle list button click
+              },
+            ),
+          ),
+
         ],
         title: Row(
           children: [
-            Container(
-              width: 40,
-              height: 40,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.message),
-                onPressed: () {
-                  // Handle message button click
-                },
-              ),
-            ),
+
             const SizedBox(width: 10),
-            Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 2.0),
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.list),
-                onPressed: () {
-                  // Handle list button click
-                },
-              ),
-            ),
+
           ],
         ),
       ),
@@ -357,7 +737,135 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ],
         ),
+
       ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // Drawer header with app icon
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Display app icon at the top center
+                  Icon(Icons.event, size: 100, color: Colors.deepPurple),
+                  SizedBox(height: 10),
+                  Text(
+                    "App Name", // Display app name
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+
+            // Menu items
+
+
+            ListTile(
+              leading: const Icon(
+                Icons.star,
+                color: Colors.yellow, // Customize the icon color as needed
+              ),
+              title: const Text("Rate Us"),
+              onTap: () {
+                showRatingDialog(context);
+              },
+            ),
+
+
+            ListTile(
+              leading: const Icon(Icons.apps,
+                color: Colors.brown,),
+              title: const Text("More Apps (Ad)"),
+              onTap: openMoreApps,
+            ),
+            ListTile(
+              leading: const Icon(Icons.share,
+                  color: Colors.lightGreen
+              ),
+              title: const Text("Share"),
+              onTap: () {
+                shareApp(context); // Pass the BuildContext to the function
+              },
+            ),
+
+
+
+            ListTile(
+              leading: const Icon(
+                Icons.feedback,
+                color: Colors.blue, // Customize the icon color as needed
+              ),
+              title: const Text("Feedback"),
+              onTap: () {
+                showFeedbackDialog(context);
+              },
+            ),
+
+// Add the "Privacy Policy" ListTile in the Drawer
+            ListTile(
+              leading: const Icon(Icons.policy),
+              title: const Text("Privacy Policy"),
+              onTap: () {
+                openPrivacyPolicyDialog(context); // Open the privacy policy dialog
+              },
+            ),
+
+
+
+            ListTile(
+              leading: const Icon(Icons.system_update),
+              title: const Text("Check For Updates"),
+              onTap: () {
+                checkForUpdates(context);
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text("Logout"),
+              onTap: () async {
+                Navigator.pop(context); // Close the Drawer
+                Navigator.push(context, MaterialPageRoute(builder: (context){return const Login_SignUp();},),);
+                await widget._auth.signOut(); // Sign out the user
+
+              },
+            ),
+
+
+
+
+
+
+
+
+
+
+
+          ],
+        ),
+      )
     );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
