cb6bc2b9fd3d6e335b3a37b3f8f2005014162629
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-09-19 21:44:32 +0530

    implement popular_events

--- lib/views/popular_events.dart
+++ lib/views/popular_events.dart
@@ -1,18 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:n_eventify/views/event_card.dart';
 
-class PopularEvents extends StatefulWidget {
+class PopularEvents extends StatelessWidget {
   const PopularEvents({super.key});
 
-  @override
-  State<PopularEvents> createState() => _PopularEventsState();
-}
-
-class _PopularEventsState extends State<PopularEvents> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Popular Events"),
+        backgroundColor: Color(0xFF00BF63),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream:
+            FirebaseFirestore.instance.collection('popular_events').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+
+          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) {
+              final event = documents[index].data() as Map<String, dynamic>;
+              return EventCard(
+                eventName: event['eventName'],
+                clubName: event['clubName'],
+                location: event['location'],
+                date: event['date'],
+                maxEntries: event['maxEntries'],
+                tags: List<String>.from(event['tags']),
+                startTime: event['startTime'],
+                endTime: event['endTime'],
+                imagePath: event['imagePath'],
+              );
+            },
+          );
+        },
       ),
     );
   }
