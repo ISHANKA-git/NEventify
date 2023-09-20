0fbbf535bc54b7abacb6ad370c70bbbc8663d77e
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-09-20 09:43:57 +0530

    add events to popular event

--- lib/views/popular_events.dart
+++ lib/views/popular_events.dart
@@ -2,45 +2,63 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:n_eventify/views/event_card.dart';
 
-class PopularEvents extends StatelessWidget {
+class PopularEvents extends StatefulWidget {
   const PopularEvents({super.key});
 
+  @override
+  State<PopularEvents> createState() => _PopularEventsState();
+}
+
+class _PopularEventsState extends State<PopularEvents> {
+  final CollectionReference _popular_events =
+      FirebaseFirestore.instance.collection("popular_events");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Popular Events"),
+        centerTitle: true,
         backgroundColor: Color(0xFF00BF63),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream:
-            FirebaseFirestore.instance.collection('popular_events').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return CircularProgressIndicator();
-          }
-
-          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (context, index) {
-              final event = documents[index].data() as Map<String, dynamic>;
-              return EventCard(
-                eventName: event['eventName'],
-                clubName: event['clubName'],
-                location: event['location'],
-                date: event['date'],
-                maxEntries: event['maxEntries'],
-                tags: List<String>.from(event['tags']),
-                startTime: event['startTime'],
-                endTime: event['endTime'],
-                imagePath: event['imagePath'],
-              );
-            },
-          );
-        },
-      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 500,
+              child: StreamBuilder(
+                stream: _popular_events.snapshots(),
+                builder: (context, AsyncSnapshot snapshots) {
+                  if (snapshots.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.green,
+                      ),
+                    );
+                  }
+                  if (snapshots.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshots.data!.docs.length,
+                        itemBuilder: (context, Index) {
+                          final DocumentSnapshot records =
+                              snapshots.data!.docs[Index];
+                          return Container(
+                            child: ListTile(
+                              title: Text(records["clubName"]),
+                              subtitle: Text(records["date"]),
+                            ),
+                          );
+                        });
+                  } else {}
+                  return Center(
+                    child: Text("There is data"),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
