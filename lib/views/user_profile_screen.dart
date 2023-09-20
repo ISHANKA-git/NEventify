ddc6b424083cb8142c0122a375e59532cc6eda72
rajithanimansa <97370413+rajithanimansa@users.noreply.github.com>
2023-09-20 09:12:22 +0530

    rajiup

--- lib/views/user_profile_screen.dart
+++ lib/views/user_profile_screen.dart
@@ -4,6 +4,9 @@ import 'package:n_eventify/views/login_signup.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 
 class UserProfileScreen extends StatefulWidget {
@@ -15,6 +18,7 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
+  File? _selectedImage;
   bool isFollowed = false;
   int followersCount = 442;
   bool isEditMode = false;
@@ -428,7 +432,57 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     );
   }
 
+  Future<void> _showImagePickerDialog() async {
+    final picker = ImagePicker();
+    final pickedImage = await showDialog<File?>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Choose Profile Picture'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.camera),
+                title: Text('Take a Picture'),
+                onTap: () async {
+                  final pickedFile =
+                  await picker.pickImage(source: ImageSource.camera);
+                  Navigator.of(context).pop(pickedFile != null
+                      ? File(pickedFile.path)
+                      : null);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text('Select from Gallery'),
+                onTap: () async {
+                  final pickedFile =
+                  await picker.pickImage(source: ImageSource.gallery);
+                  Navigator.of(context).pop(pickedFile != null
+                      ? File(pickedFile.path)
+                      : null);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.cancel),
+                title: Text('Remove Current Picture'),
+                onTap: () {
+                  Navigator.of(context).pop(null);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
 
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = pickedImage;
+      });
+    }
+  }
 
 
 
@@ -441,26 +495,13 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   final AuthServices _auth = AuthServices();
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+        key: _scaffoldKey, // Add this line to assign the GlobalKey
       appBar: AppBar(
         backgroundColor: Colors.white,
         actions: [
-
-          Container(
-            width: 40,
-            height: 40,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-            ),
-            child: IconButton(
-              icon: const Icon(Icons.message, color: Colors.greenAccent),
-              onPressed: () {
-                Scaffold.of(context).openDrawer(); // Handle message button click
-              },
-             
-            ),
-          ),
           const SizedBox(width: 10),
           Container(
             width: 40,
@@ -470,12 +511,36 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               border: Border.all(color: Colors.white, width: 2.0),
             ),
             child: IconButton(
-              icon: const Icon(Icons.list,color: Colors.greenAccent),
+              icon: const Icon(Icons.textsms,color: Colors.greenAccent),
               onPressed: () {
-                // Handle list button click
+                Scaffold.of(context).openDrawer(); // Handle list button click
               },
             ),
           ),
+
+
+
+          const SizedBox(width: 10),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.white, width: 2.0),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.view_list_outlined,color: Colors.greenAccent),
+              onPressed: () {
+                _scaffoldKey.currentState?.openEndDrawer(); // Open the end drawer
+              },
+            ),
+          ), // Create a GestureDetector for the right side corner
+
+
+
+
+
+
 
         ],
         title: Row(
@@ -514,7 +579,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                       child: IconButton(
                         icon: Icon(
                           isEditMode ? Icons.done : Icons.edit,
-                          color: Colors.greenAccent,
+                          color: Colors.blueGrey,
                         ),
                         onPressed: () {
                           // Handle edit button click
@@ -526,17 +591,21 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Container(
+
+
                           width: 100,
                           height: 100,
                           decoration: BoxDecoration(
                             color: Colors.green,
                             borderRadius: BorderRadius.circular(15),
                           ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Image.asset(
-                              'assets/logo_withname.png',
-                              fit: BoxFit.cover,
+                          child: GestureDetector(
+                            onTap: _showImagePickerDialog,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: _selectedImage != null
+                                  ? Image.file(_selectedImage!, fit: BoxFit.cover)
+                                  : Image.asset('assets/thammu.png', fit: BoxFit.cover),
                             ),
                           ),
                         ),
@@ -639,12 +708,12 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                             ElevatedButton(
                               onPressed: toggleFollow,
                               style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.green,
+                                backgroundColor: Colors.lightGreen,
                                 elevation: 0,
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(20),
                                   side: const BorderSide(
-                                    color: Colors.green,
+                                    color: Colors.white,
                                     width: 2,
                                   ),
                                 ),
@@ -740,7 +809,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
       ),
 
-      drawer: Drawer(
+    endDrawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
