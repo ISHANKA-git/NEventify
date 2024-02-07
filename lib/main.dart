6d3aefc62cc4d8bd2e1fbf4a39c4d9e2d86d560c
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2024-02-07 10:34:45 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:n_eventify/controller/auth.dart';
-import 'package:n_eventify/controller/wrapper.dart';
 import 'package:n_eventify/model/user_model.dart';
-import 'package:n_eventify/views/bottom_navigation_bar.dart';
-import 'package:n_eventify/views/login_signup.dart';
-import 'package:n_eventify/views/home_page.dart';
-import 'package:n_eventify/views/onboarding_screen.dart';
-import 'package:n_eventify/views/event_creation_page.dart';
 import 'package:n_eventify/views/splash_page.dart';
-import 'package:n_eventify/views/chat_list_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -33,12 +26,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<UserModel?>.value(
-        initialData: UserModel(uid: ""),
-        value: AuthServices().user,
-        child : MaterialApp(
+      initialData: UserModel(uid: ""),
+      value: AuthServices().user,
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: SplashPage(),
-        ),
+      ),
     );
   }
 }
